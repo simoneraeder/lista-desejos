39d2852483b9271cdb331210c0da4ac6e6d1f3e9
Simone <simoneraeder@live.com>
2025-06-27 21:31:20 -0300

    react

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,26 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
 export const Footer = () => {
-    return (
-       <footer className={styles.footer}>
-        <div className={styles.container} >
-            <div className={styles.content}>
-            <p className={styles.dir}> 2025, Meus Desejos - Todos os direitos reservados</p>
-                    <div className={styles.links}>
-                        <a className={styles.githubLink}  href="https://github.com/simoneraeder" target="_blank" rel="noopener noreferrer">Simone Raeder</a>
-                    </div>
-            </div>
-         
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <p className={styles.dir}>
+            {" "}
+            2025, Meus Desejos - Todos os direitos reservados
+          </p>
+          <div className={styles.links}>
+            <a
+              className={styles.githubLink}
+              href="https://github.com/simoneraeder"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Simone Raeder
+            </a>
+          </div>
         </div>
-        
-       </footer>
-    )
-}
+      </div>
+    </footer>
+  );
+};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
 export const Header = () => {
-    return(
-       <header className={styles.header}>
-        <div className={styles.container}>
-            <h1 className={styles.title}>Meus desejos</h1>
-        </div>
-       </header>
-    )
-}
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Meus desejos</h1>
+      </div>
+    </header>
+  );
+};
