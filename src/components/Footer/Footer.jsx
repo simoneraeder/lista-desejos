f97c1705904e3738b8c6fcf84ccb6035e0e5e37c
Simone <simoneraeder@live.com>
2025-06-27 21:15:21 -0300

    react

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,18 @@
+import styles from './Footer.module.css'
+
 export const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+       <footer className={styles.footer}>
+        <div className={styles.container} >
+            <div className={styles.content}>
+            <p className={styles.dir}> 2025, Meus Desejos - Todos os direitos reservados</p>
+                    <div className={styles.links}>
+                        <a className={styles.githubLink}  href="https://github.com/simoneraeder" target="_blank" rel="noopener noreferrer">Simone Raeder</a>
+                    </div>
+            </div>
+         
         </div>
+        
+       </footer>
     )
 }
