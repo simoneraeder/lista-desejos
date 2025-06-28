27b511821eb81c9e3cbbcec171175077f7ae348a
Simone <simoneraeder@live.com>
2025-06-27 21:43:59 -0300

    REACT

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-
-import styles from './App.module.css'
-import { Card } from './components/Card/Card'
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
+import styles from "./App.module.css";
+import { Card } from "./components/Card/Card";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
 
 function App() {
-
   return (
-   <div className={styles.app}>
-    <Header/>
-    <main className={styles.main}>
-      <Card />
-    </main>
-    <Footer/>
-   </div>
-  )
+    <div className={styles.app}>
+      <Header />
+      <main className={styles.main}>
+        <Card name="PS5" DE />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,15 @@
-export const Card = () => {
-    return (
-        <div>
-            <h1>Card</h1>
-        </div>
-    )
-}
+import styles from "./Card.module.css";
+
+export const Card = ({ name, description, image }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.imgContainer}>
+        <img src={image} alt={name} className={styles.image} />
+      </div>
+      <div className={styles.content}>
+        <h3 className={styles.name}>{name}</h3>
+        <p className={styles.descripton}>{descripton}</p>
+      </div>
+    </div>
+  );
+};
