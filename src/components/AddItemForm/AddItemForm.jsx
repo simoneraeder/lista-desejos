9629857818da130e230c3fabfff4ae54e8a33b45
Simone Raeder Blasse Gorni <PET0099063@aluno.firjansenaisesi.com.br>
2025-07-02 19:33:23 -0300

    atualizado

--- src/components/AddItemForm/AddItemForm.jsx
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./AddItemForm.module.css";
 export const AddItemForm = ({handleSubmit, form, setForm}) =>{
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -9,35 +10,38 @@ export const AddItemForm = ({handleSubmit, form, setForm}) =>{
       };
     return (
         <>
-        <form onSubmit={handleSubmit}>
-          <div>
+        <form className={styles.container} onSubmit={handleSubmit}>
+          <div className={styles.inputContainer}>
             <label htmlFor="name">Desejo</label>
             <input
               type="text"
               name="name"
               value={form.name}
               onChange={handleChange}
+              required
             />
           </div>
-          <div>
+          <div className={styles.inputContainer}>
             <label htmlFor="description">Descrição</label>
             <input
               type="text"
               name="description"
               value={form.description}
               onChange={handleChange}
+              required
             />
           </div>
-          <div>
+          <div className={styles.inputContainer}>
             <label htmlFor="">URL Imagem</label>
             <input
               type="text"
               name="urlImage"
               value={form.urlImage}
               onChange={handleChange}
+              required
             />
           </div>
-          <button type="submit">Adicionar</button>
+          <button className={styles.button} type="submit">Adicionar</button>
         </form>
         </>
       );
