a1bb7d27d895c5d565ced62fe75725c37ae3307b
Simone Raeder Blasse Gorni <PET0099063@aluno.firjansenaisesi.com.br>
2025-07-04 20:06:04 -0300

    atualizando

---
+++ src/components/EmptyState/EmptyState.jsx
@@ -0,0 +1,9 @@
+import styles from "./EmptyState.module.css";
+
+export const EmpyState = () => {
+  return (
+    <>
+      <div className={styles.container}>Sem desejos cadastrados</div>
+    </>
+  );
+};
