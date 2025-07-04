e95ebd157760fd000161fba50952ec986c7da072
Simone Raeder Blasse Gorni <PET0099063@aluno.firjansenaisesi.com.br>
2025-07-04 19:02:40 -0300

    atualizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,7 @@
 import { useState, useCallback } from "react";
 import styles from "./Header.module.css";
 
-
-export const Header = ({onSearch, onClear}) => {
+export const Header = ({ onSearch, onClear }) => {
   const [searchValue, setSearchValue] = useState("");
 
   const handleSubmit = useCallback(
@@ -13,30 +12,32 @@ export const Header = ({onSearch, onClear}) => {
     [searchValue, onSearch]
   );
 
-  const handleChange = useCallback(
-    (e) => {
-      const value = e.target.value;
-      setSearchValue(value);
-      }, []);
+  const handleChange = useCallback((e) => {
+    const value = e.target.value;
+    setSearchValue(value);
+  }, []);
 
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <h1 className={styles.title}>Meus desejos</h1>
-        <form className= {styles.searchForm} onSubmit={handleSubmit}>
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
           <div className={styles.searchGroup}>
-<input
- type="text" 
- placeholder="Pesquisar desejos..."
- value={searchValue}
-  onChange={handleChange} 
-  className={styles.input} 
-  />
-  <button type="submit" className={styles.button}>Pesquisar</button>
-  <button type="button" onClick={onClear}
-   className={styles.clear}>
-    Limpar
-    </button>
+            <input
+              type="text"
+              placeholder="Pesquisar desejos..."
+              value={searchValue}
+              onChange={handleChange}
+              className={styles.input}
+            />
+            <div className={styles.actions}>
+              <button type="submit" className={styles.button}>
+                Pesquisar
+              </button>
+              <button type="button" onClick={onClear} className={styles.clear}>
+                Limpar
+              </button>
+            </div>
           </div>
         </form>
       </div>
